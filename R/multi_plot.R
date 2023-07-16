7dac3f81d2da292ce9eb0bf18be5639683ec3f31
Devon <88951175+dev-grossett@users.noreply.github.com>
2023-07-16 17:05:00 +1200

    Initial commit

---
+++ R/multi_plot.R
@@ -0,0 +1,39 @@
+#' Plots exposures and frequency by given variables
+#'
+#' @param dat input data set
+#' @param vars vector of variable names in character format i.e
+#' `c("var1, "var2")`
+#' @param claim_var column that represents the number of claims for an obs
+#' @param exposure_var column that represents the number of exposures for an obs
+#'
+#'
+#' @return ggplots of exposures and frequency, grouped by the variables in vars
+#'
+multi_plot <- function(dat, vars, claim_var = "ClaimNb",
+                       exposure_var = "Exposure") {
+
+  for (i in 1:length(vars)) {
+    var = vars[i]
+
+    claims <- rlang::ensym(claim_var)
+    exposures <- rlang::ensym(exposure_var)
+
+    new_dat <- dat %>%
+      dplyr::group_by(!! rlang::ensym(var)) %>%
+      dplyr::summarise(Claims = sum((!! claims)),
+                       Exposures = sum(!! exposures)) %>%
+      dplyr::mutate(Freq = Claims / Exposures)
+
+    p1 <- ggplot2::ggplot(new_dat, ggplot2::aes(x = !! rlang::ensym(var),
+                                       weight = Exposures)) +
+      ggplot2::geom_bar() +
+      ggplot2::ggtitle(var)
+
+    p2 <- ggplot2::ggplot(new_dat, ggplot2::aes(x = !! rlang::ensym(var),
+                                                y = Freq)) +
+      ggplot2::geom_point() +
+      ggplot2::geom_smooth()
+
+    gridExtra::grid.arrange(p1, p2, nrow = 1)
+  }
+}
---
+++ R/scoring_function.R
@@ -0,0 +1,55 @@
+#' LightGBM training score
+#'
+#' Calculates the training score of a LightGBM model using n-fold cross
+#' validation. Useful for tuning hyperparameters
+#'
+#' @param training_data Dataset to train model on
+#' @param num_leaves Maximum number of leaves in one tree
+#' @param min_data_in_leaf Minimum number of observations one leaf can be based
+#'  off
+#' @param num_trees Number of boosting iterations
+#' @param learning_rate Shrinkage rate - determines how quickly the ensemble
+#' learns
+#' @param bagging_freq Frequency for bagging (boostrap aggregation)
+#' @param bagging_fraction Proportion of data to use for training each tree
+#' @param feature_fraction Proportion of variables to use for training each tree
+#' @param objective Loss function - see LightGBM docs for full list available
+#' @param nfolds number of folds for cross validation score
+#' @param early_stopping_rounds number of rounds to stop if no improvement in
+#' cross validation score
+#'
+#' @return List containing the Best Score against the `objective` and how many
+#' iterations based on early stopping
+scoring_function <- function(training_data,
+                             num_leaves = 2L,
+                             min_data_in_leaf = 20L,
+                             num_trees = 4000L,
+                             learning_rate = 0.01,
+                             bagging_freq = 1L,
+                             bagging_fraction = 0.5,
+                             feature_fraction = 0.6,
+                             objective = "poisson",
+                             nfolds = 3L,
+                             early_stopping_rounds = 100L) {
+
+  pars <- list(num_leaves = num_leaves,
+               min_data_in_leaf = min_data_in_leaf,
+               num_trees = num_trees,
+               learning_rate = learning_rate,
+               bagging_freq = bagging_freq,
+               bagging_fraction = bagging_fraction,
+               feature_fraction = feature_fraction,
+               objective = objective,
+               deterministic = TRUE) #attempting to make reproducible
+
+  lgb_cv <- lightgbm::lgb.cv(params = pars,
+                            data = training_data,
+                            nfold = nfolds,
+                            verbose = -100,
+                            # early stopping instead of explicitly training for
+                            # num_trees
+                        early_stopping_rounds = early_stopping_rounds)
+
+  return(list(Score = lgb_cv$best_score,
+              Trees = lgb_cv$best_iter))
+}
